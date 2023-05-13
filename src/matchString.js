cf13661b481e752fd6ff4e865301d11ec61f9535
Vidita Kumari <kvidita@users.noreply.github.com>
2023-05-13 21:30:47 +0530

    using arrow functions

--- src/matchString.js
+++ src/matchString.js
@@ -1,9 +1,7 @@
 const textToArray = (text) => text.trim().split("\n");
 
 const searchMatch = function (string, list) {
-  return list.find(function (element) {
-    return string === element.slice(0, 4);
-  });
+  return list.find((element) => string === element.slice(0, 4));
 };
 
 const createList = function (list1, list2) {
